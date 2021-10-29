699fba8f6f0cb02a885b3e7ad82ad015817f0239
Miker <michaelirick@gmail.com>
2021-10-28 22:47:42 -0500

    added controllers

---
+++ app/controllers/forecast_controller.rb
@@ -0,0 +1,7 @@
+class ForecastController < ApplicationController
+  def index
+  end
+  
+  def forecast
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # post 'forecast/forecast', as: :get_forecast
+  # get 'forecast#index', as: :forecast_index
+
+  root to: 'forecast#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
